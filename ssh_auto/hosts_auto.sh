f698f970b1de4b384c256012cd84f680fba0272a
root <root@ip-172-31-42-139.us-east-2.compute.internal>
2018-07-03 21:37:12 +0000

    hosts updated

--- ssh_auto/hosts_auto.sh
+++ ssh_auto/hosts_auto.sh
@@ -4,13 +4,13 @@ aws_region="us-east-2"
 
 echo "[prometheus_server]" >>../hosts
 
-elk=`aws ec2 describe-instances --region $aws_region --filters "Name=tag:Name,Values=prometheus_server" --query "Reservations[*].Instances[*].PublicIpAddress" --output=text`
+prometheus_server=`aws ec2 describe-instances --region $aws_region --filters "Name=tag:Name,Values=prometheus_server" --query "Reservations[*].Instances[*].PublicIpAddress" --output=text`
 
-echo "$elk ansible_ssh_user=ec2-user ansible_ssh_private_key_file=/ec2-user/.ssh/id_rsa" >> ../hosts
+echo "$prometheus_server ansible_ssh_user=ec2-user ansible_ssh_private_key_file=/ec2-user/.ssh/id_rsa" >> ../hosts
 
 
 echo "[prometheus_client]" >> ../hosts
 
-elk_client=`aws ec2 describe-instances --region $aws_region --filters "Name=tag:Name,Values=phpapp1" --query "Reservations[*].Instances[*].PublicIpAddress" --output=text`
+prometheus_client=`aws ec2 describe-instances --region $aws_region --filters "Name=tag:Name,Values=phpapp1" --query "Reservations[*].Instances[*].PublicIpAddress" --output=text`
 
-echo "$elk_client ansible_ssh_user=ec2-user ansible_ssh_private_key_file=/ec2-user/.ssh/id_rsa" >> ../hosts
+echo "$prometheus_client ansible_ssh_user=ec2-user ansible_ssh_private_key_file=/ec2-user/.ssh/id_rsa" >> ../hosts
