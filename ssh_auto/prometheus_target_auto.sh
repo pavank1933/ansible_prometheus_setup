e1f2ea0a5dcc3b2a567ddb66db10ae59750480aa
root <root@ip-172-31-42-139.us-east-2.compute.internal>
2018-07-03 21:22:11 +0000

    soft

--- ssh_auto/prometheus_target_auto.sh
+++ ssh_auto/prometheus_target_auto.sh
@@ -2,6 +2,6 @@
 
 aws_region="us-east-2"
 phpapp1=`aws ec2 describe-instances --region $aws_region --filters "Name=tag:Name,Values=prometheus_client" --query "Reservations[*].Instances[*].PublicIpAddress" --output=text`
-sed -e "s/\${promc1}/$phpapp1/"  prometheus.yml >> test.yml
-rm -rf prometheus.yml
-mv test.yml prometheus.yml
+sed -e "s/\${promc1}/$phpapp1/"  ../prometheus_server/templates/prometheus.yml >> ../prometheus_server/templates/test.yml
+rm -rf ../prometheus_server/templates/prometheus.yml
+mv ../prometheus_server/templates/test.yml ../prometheus_server/templates/prometheus.yml
