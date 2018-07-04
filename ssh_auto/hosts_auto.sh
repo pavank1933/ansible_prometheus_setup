5316a5b1c6067118fd928d53a3fb3716bef813db
root <root@ip-172-31-30-117.us-east-2.compute.internal>
2018-07-04 15:21:43 +0000

    updated hosts file

--- ssh_auto/hosts_auto.sh
+++ ssh_auto/hosts_auto.sh
@@ -6,11 +6,11 @@ echo "[prometheus_server]" >>../hosts
 
 prometheus_server=`aws ec2 describe-instances --region $aws_region --filters "Name=tag:Name,Values=prometheus_server" --query "Reservations[*].Instances[*].PublicIpAddress" --output=text`
 
-echo "$prometheus_server ansible_ssh_user=ec2-user ansible_ssh_private_key_file=/ec2-user/.ssh/id_rsa" >> ../hosts
+echo "$prometheus_server ansible_ssh_user=ec2-user ansible_ssh_private_key_file=/root/.ssh/id_rsa" >> ../hosts
 
 
 echo "[prometheus_client]" >> ../hosts
 
 prometheus_client=`aws ec2 describe-instances --region $aws_region --filters "Name=tag:Name,Values=phpapp1" --query "Reservations[*].Instances[*].PublicIpAddress" --output=text`
 
-echo "$prometheus_client ansible_ssh_user=ec2-user ansible_ssh_private_key_file=/ec2-user/.ssh/id_rsa" >> ../hosts
+echo "$prometheus_client ansible_ssh_user=ec2-user ansible_ssh_private_key_file=/root/.ssh/id_rsa" >> ../hosts
